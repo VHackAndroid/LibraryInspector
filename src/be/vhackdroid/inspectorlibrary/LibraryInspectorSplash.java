5855915e3f04922c1074bb0bb4115aa4d28c8e01
Stephan Celis <stephan.celis@gmail.com>
2012-10-05 23:39:01 +0200

    First commit

---
+++ src/be/vhackdroid/inspectorlibrary/LibraryInspectorSplash.java
@@ -0,0 +1,13 @@
+package be.vhackdroid.inspectorlibrary;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LibraryInspectorSplash extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
