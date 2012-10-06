72f644e2f62a323edcb2f7d00d48822fde4ff31b
jacobs.joris@hotmail.com <jacobs.joris@hotmail.com>
2012-10-06 20:20:48 +0200

    edited dummy

--- gen/be/vhackdroid/inspectorlibrary/views/R.java
+++ gen/be/vhackdroid/inspectorlibrary/views/R.java
@@ -38,27 +38,30 @@ public final class R {
         public static final int title=0x7f02000e;
     }
     public static final class id {
+        public static final int button1=0x7f070005;
+        public static final int checkBox1=0x7f070003;
         public static final int introComic=0x7f070000;
         public static final int introComicArrow=0x7f070001;
-        public static final int textView_test=0x7f070002;
-        public static final int themesButton1=0x7f070005;
-        public static final int themesButton2=0x7f070008;
-        public static final int themesButton3=0x7f07000b;
-        public static final int themesButton4=0x7f07000e;
-        public static final int themesButton5=0x7f070011;
-        public static final int themesButton6=0x7f070014;
-        public static final int themesKey1=0x7f070003;
-        public static final int themesKey2=0x7f070006;
-        public static final int themesKey3=0x7f070009;
-        public static final int themesKey4=0x7f07000c;
-        public static final int themesKey5=0x7f07000f;
-        public static final int themesKey6=0x7f070012;
-        public static final int themesTitle1=0x7f070004;
-        public static final int themesTitle2=0x7f070007;
-        public static final int themesTitle3=0x7f07000a;
-        public static final int themesTitle4=0x7f07000d;
-        public static final int themesTitle5=0x7f070010;
-        public static final int themesTitle6=0x7f070013;
+        public static final int linearLayout1=0x7f070002;
+        public static final int textView_test=0x7f070004;
+        public static final int themesButton1=0x7f070008;
+        public static final int themesButton2=0x7f07000b;
+        public static final int themesButton3=0x7f07000e;
+        public static final int themesButton4=0x7f070011;
+        public static final int themesButton5=0x7f070014;
+        public static final int themesButton6=0x7f070017;
+        public static final int themesKey1=0x7f070006;
+        public static final int themesKey2=0x7f070009;
+        public static final int themesKey3=0x7f07000c;
+        public static final int themesKey4=0x7f07000f;
+        public static final int themesKey5=0x7f070012;
+        public static final int themesKey6=0x7f070015;
+        public static final int themesTitle1=0x7f070007;
+        public static final int themesTitle2=0x7f07000a;
+        public static final int themesTitle3=0x7f07000d;
+        public static final int themesTitle4=0x7f070010;
+        public static final int themesTitle5=0x7f070013;
+        public static final int themesTitle6=0x7f070016;
     }
     public static final class layout {
         public static final int introscreen=0x7f030000;
