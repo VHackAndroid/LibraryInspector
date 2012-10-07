422c88b874b8a91f23aaabe3bd46c34be181d64b
Stephan Celis <stephan.celis@gmail.com>
2012-10-07 03:02:22 +0200

    update UI muziek story

--- src/be/vhackdroid/inspectorlibrary/views/LibraryInspectorThemeMuziek.java
+++ src/be/vhackdroid/inspectorlibrary/views/LibraryInspectorThemeMuziek.java
@@ -1,12 +1,27 @@
 package be.vhackdroid.inspectorlibrary.views;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
 public class LibraryInspectorThemeMuziek extends LibraryInspectorNfcDummy {
 
+	private Context context;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.thememuziek);
+		context = this;
+		ImageView helpButton = (ImageView) findViewById(R.id.themesMuziekHelpKnop);
+		helpButton.setOnClickListener(new OnClickListener() {			
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(context, LibraryInspectorThemeMuziekHelp.class));
+			}
+		});
 	}
 }
