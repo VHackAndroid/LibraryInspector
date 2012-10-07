ffc16f5b9e4d0e28026f232813292de3560a6f0e
Stephan Celis <stephan.celis@gmail.com>
2012-10-07 14:22:14 +0200

    scherm voor het einde

---
+++ src/be/vhackdroid/inspectorlibrary/views/LibraryInspectorEinde.java
@@ -0,0 +1,55 @@
+package be.vhackdroid.inspectorlibrary.views;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+
+public class LibraryInspectorEinde extends LibraryInspectorNfcDummy {
+	
+	private int currentImage = -1;
+	private Bitmap[] images = new Bitmap[4];
+	
+	@Override
+	public void onChildTitleChanged(Activity childActivity,
+			CharSequence title) {
+		super.onChildTitleChanged(childActivity, title);
+		setContentView(R.layout.einde);
+		
+		// Init Images
+		images[0] = BitmapFactory.decodeResource(getResources(),
+				R.drawable.einde01);
+		images[1] = BitmapFactory.decodeResource(getResources(),
+				R.drawable.einde02);
+		images[2] = BitmapFactory.decodeResource(getResources(),
+				R.drawable.einde03);
+		images[3] = BitmapFactory.decodeResource(getResources(),
+				R.drawable.einde04);
+
+		ImageView arrow = (ImageView) findViewById(R.id.EindeArrow);
+		arrow.setOnClickListener(new ArrowClicker());
+
+		setNextImage();
+	}
+	
+	private void setNextImage() {
+		currentImage++;
+		if (currentImage < images.length) {
+			ImageView imageView = (ImageView) findViewById(R.id.eindeComic);
+			imageView.setImageBitmap(images[currentImage]);
+		} else {
+			finish();
+			startActivity(new Intent(this, LibraryInspectorThemes.class));
+		}
+	}
+
+	private class ArrowClicker implements OnClickListener {
+		@Override
+		public void onClick(View v) {
+			setNextImage();
+		}
+	}
+}
